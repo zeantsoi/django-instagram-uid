e6a5c1db5f16db0c7f61a6e2b3c85e587ee60e11
Zean Tsoi <zeantsoi@elb013951.(none)>
2013-01-01 15:59:13 -0700

    Adding documentation

--- lookup/views.py
+++ lookup/views.py
@@ -8,17 +8,25 @@ from instagram.client import InstagramAPI
 import django_instagram_uid.settings as settings
 
 def index(request, access_token_error=None, username_error=None): 
+
+	# Verify that the request is a POST
 	if request.method == 'POST':
+		
+		# If the access_token argument is empty, use the access token from setting.py; otherwise use the access_token argument
 		if request.POST['access_token']:
 			access_token = request.POST['access_token']
 		else:
 			access_token = settings.INSTAGRAM_ACCESS_TOKEN
 		
+		# Initialize Instagram library with access_token
 		api = InstagramAPI(access_token=access_token)
 
 		if request.POST['username']:
+			# Find users matching username (should only be one)
 			try:
 				users = api.user_search(request.POST['username'])
+
+			# Rescue exception if access_token is incorrect
 			except:
 				if request.POST['access_token']:
 					access_token_error = "The Access Token entered is invalid"
@@ -26,6 +34,7 @@ def index(request, access_token_error=None, username_error=None):
 					access_token_error = "The Access Token in settings.py is invalid"
 
 		try:
+			# Map attributes of the first and only user in the users list
 			user = users[0]
 			attributes = {
 				'id': user.id,
@@ -35,29 +44,44 @@ def index(request, access_token_error=None, username_error=None):
 				'username': user.username,
 				'website': user.website,
 			}
+
+			# If any value in the attributes dict is blank, then remove the key value pair from the dict
 			attributes = dict((k,v) for k,v in attributes.items() if v)
+		
+		# If users array is empty, then no matching users were found
 		except:
 			username_error = "The username entered is not valid"
 
 		if request.is_ajax():
+			
+			# If an exception occured, determine which error message should be displayed
 			if (access_token_error or username_error):
 				if access_token_error:
 					error = access_token_error
 				elif username_error:
 					error = username_error
 
+				# Return error message to template
 				return HttpResponse(json.dumps({"error":(error)}), mimetype="application/json")
+			
+			# If lookup request is successful, return attributes to template
 			else:
-				
 				return HttpResponse(json.dumps(attributes), mimetype="application/json")
+		# If POST request isn't AJAX, manually assign view context
 		else:
 			context = {}
+			
+			# If an exception occured, assign an error to the context, otherwise assign the attributes dict to the context
 			if access_token_error:
 				context.update({'error':access_token_error})
 			elif username_error:
 				context.update({'error':username_error})
 			else:
 				context = attributes
+
+			# Return view context to template
 			return render(request, 'index.html', context)
+	
+	# If request isn't POST, then display the template
 	else:
 		return render(request, 'index.html')
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,34 +1,60 @@
 $(function() {
+
+	// Initiate Javascript callback upon form submission
 	$('form[id="lookup"]').submit(function(event) {
+
+		// Prevent form from submitting via HTML submit
 		event.preventDefault();
+
+		// Clear right hand form upon form submission
 		clearData();
+
+		// Show loading gif
 		$('img#loading').show();
+		
+		// AJAX form submission
 		$.ajax({
 			type: 'POST',
 			url: "/",
 			data: {
+				// Add Django CSRF token to request
 				'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val(),
 				'username': $('input[name="username"]').val(),
 				'access_token': $('input[name="access_token"]').val(),
 			},
 			success: function(data){
+
+				// Hide loading gif
 				$('img#loading').hide();
+
+				// Display error message if there was an exception
 				if (data['error']) {
 					$('.uid > h4').html(data['error']);
 					$('.uid > h4').fadeIn();
+
+				// Load attributes if request was successful
 				} else {
+				
+					// Load id into the DOM and display
 					$('.uid > h3').html(data['id']);
 	 				$('.uid > h3').fadeIn();
+				
+					// Initialize html string var and begin building html block
 					html = '<img src="' + data['profile_picture'] + '" alt="' + data['username'] + '" /><div class="info"><ul>';	 				
 	 				attributes = ['full_name', 'website', 'bio'];
+
+					// For each attribute in attributes list, map and format into html if there's a key that matches the attribute
 	 				attributes.map(function(attribute) {
 	 					if (data[attribute]) html += '<li>' + data[attribute] + '</li>';
 	 				});
+
+	 				// End html block and load into the DOM
 	 				$('.attributes').html(html += "</ul></div>");
+	 				
+	 				// Show the attributes div
 	 				$('.attributes').fadeIn();
-
+				
 				}
-
 			}
 		});
 	});
